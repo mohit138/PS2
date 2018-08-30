4399e9bed2d132dc66c08f7c07ba867bfc22e1ef
mohit138 <39694943+mohit138@users.noreply.github.com>
2018-08-31 02:39:11 +0530

    combination
    button combination issue solved

---
+++ ps2.ino
@@ -0,0 +1,360 @@
+#include <SPI.h>
+   
+int Confidencelevel[5];
+enum {x,l1,l2,r1,r2};
+int L1counter=0,L2counter=0;
+int xflag=0,l1flag=0,l2flag=0,r1flag=0,r2flag=0;
+int xhold=0,l1hold=0,l2hold=0,r1hold=0,r2hold=0;
+/*void scalePS2value(){
+      ySquare_leftdistance=(basePS2_Y-ps2x.Analog(PSS_LY))/basePS2_Y;
+      xSquare_leftdistance=(-ps2x.Analog(PSS_LX)+128)/128.0;
+      ySquare_rightdistance=(basePS2_Y-ps2x.Analog(PSS_RY))/basePS2_Y;
+      xSquare_rightdistance=(-ps2x.Analog(PSS_RX)+128)/128.0;
+      if(xSquare_leftdistance==0 && ySquare_leftdistance==0){
+      xCircle_leftdistance=0;
+      yCircle_leftdistance=0; 
+      }
+      else{
+      xCircle_leftdistance= xSquare_leftdistance *sqrt(pow(xSquare_leftdistance,2)+pow(ySquare_leftdistance,2)-(pow(xSquare_leftdistance,2)*pow(ySquare_leftdistance,2)))/sqrt(pow(xSquare_leftdistance,2)+pow(ySquare_leftdistance,2));
+      yCircle_leftdistance= ySquare_leftdistance *sqrt(pow(xSquare_leftdistance,2)+pow(ySquare_leftdistance,2)-(pow(xSquare_leftdistance,2)*pow(ySquare_leftdistance,2)))/sqrt(pow(xSquare_leftdistance,2)+pow(ySquare_leftdistance,2));
+      }
+      if(xSquare_rightdistance==0 && ySquare_rightdistance==0){
+      xCircle_rightdistance=0;
+      yCircle_rightdistance=0; 
+      }
+      else{
+      xCircle_rightdistance= xSquare_rightdistance *sqrt(pow(xSquare_rightdistance,2)+pow(ySquare_rightdistance,2)-(pow(xSquare_rightdistance,2)*pow(ySquare_rightdistance,2)))/sqrt(pow(xSquare_rightdistance,2)+pow(ySquare_rightdistance,2));
+      yCircle_rightdistance= ySquare_rightdistance *sqrt(pow(xSquare_rightdistance,2)+pow(ySquare_rightdistance,2)-(pow(xSquare_rightdistance,2)*pow(ySquare_rightdistance,2)))/sqrt(pow(xSquare_rightdistance,2)+pow(ySquare_rightdistance,2));
+      }
+      
+      LeftAnalogDistance=sqrt((xCircle_leftdistance)*(xCircle_leftdistance)+(yCircle_leftdistance)*(yCircle_leftdistance));
+      LeftAnalogTheta=atan2(yCircle_leftdistance,xCircle_leftdistance)*(180.0/3.14);
+      RightAnalogDistance=sqrt((xCircle_rightdistance)*(xCircle_rightdistance)+(yCircle_rightdistance)*(yCircle_rightdistance));
+      RightAnalogTheta=atan2(yCircle_rightdistance,xCircle_rightdistance)*(180.0/3.14);
+} 
+
+*/
+void initPS2()
+{
+   error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, true, true);
+  if(error == 0)
+    Serial.print("Found Controller, configured successful ");
+  else if(error == 1)
+    Serial.println("No controller found"); 
+  
+  type = ps2x.readType(); 
+  
+  switch(type) {
+    case 0:
+      Serial.print("Unknown Controller type found ");
+      break;
+    case 1:
+      Serial.print("DualShock Controller found ");
+      break;
+    case 2: 
+      Serial.print("GuitarHero Controller found ");
+      break;
+    case 3:
+      Serial.print("Wireless Sony DualShock Controller found ");
+      break;
+  }
+}
+
+
+
+
+
+void getPS2value()
+{ 
+  ps2x.read_gamepad(false,vibrate);          //read controller  
+}
+
+void rightPressed(){  
+  SerialPS2.write('r');
+}  
+void rightReleased(){
+}
+void leftPressed(){
+  SerialPS2.write('l');
+  //SerialPS2.print('l');
+} 
+void leftReleased(){
+  
+}
+void upPressed(){
+  frontpressed = 1;
+}
+void upReleased(){
+  frontpressed=0;  
+}
+void downPressed(){
+  backpressed=1;
+} 
+void downReleased(){
+  backpressed=0;
+}
+void selPressed(){
+}
+void selReleased(){
+}
+void startPressed(){
+}
+void startReleased(){  
+}
+void trianglePressed(){
+    SerialPS2.write('s');
+    
+}
+void triangleReleased(){
+}
+void circleReleased(){
+}
+ void circlePressed(){
+}
+void crossPressed(){
+    Serial.println('x');
+}
+void crossReleased(){
+}
+void squarePressed(){
+}  
+void squareReleased(){
+}
+
+void l1Pressed(){
+Serial.println("l1");
+}
+void l1Released(){  
+}  
+void r1Released(){
+}
+void r1Pressed(){
+}  
+void l2Released(){
+}
+void l2Pressed(){
+Serial.println("l2");
+}
+void r2Released(){
+}
+void r2Pressed(){
+  SerialPS2.write('w');
+}
+void L1L2pressed(){
+Serial.println("l1l2c");
+}
+                
+void PS2executePressed(){
+
+    if(ps2x.ButtonReleased(PSB_L1))
+     {
+       l1hold=0;
+     
+     }   
+     
+     if(ps2x.ButtonPressed(PSB_L1))
+     {
+       l1hold=1;
+      }
+     
+     
+     if(ps2x.ButtonPressed(PSB_L2))
+     {    
+          l2hold=1;
+    }
+
+    if(ps2x.ButtonReleased(PSB_L2))
+    {
+        l2hold=0;
+        
+    }   
+    if(ps2x.Button(PSB_L1) && l1hold==1){
+      L1counter++;
+        if(L1counter>10){
+          if(ps2x.Button(PSB_L2) && l2hold==1 && l1hold==1){
+            L2counter++;
+              if(L2counter>10){
+                L1L2pressed();
+                l1hold=0;
+                L1counter=0;
+                L2counter=0;
+                l2hold=0;
+              }  
+          }
+          if(L1counter > 10/*900000*/ && L2counter==0 && l1hold==1 && l2hold==0){
+            l1Pressed();
+            L1counter=0;
+            L2counter=0;
+            l1hold=0;
+            l2hold=0;
+          }
+        }
+    }
+       
+    if(ps2x.Button(PSB_L2) && l2hold==1){
+      L2counter++;
+        if(L2counter>10){
+          if(ps2x.Button(PSB_L1) && l1hold==1 && l2hold==1){
+            L1counter++;
+              if(L1counter>10){
+                L1L2pressed();
+                l1hold=0;
+                L1counter=0;
+                L2counter=0;
+                l2hold=0;
+              }  
+          }
+          if(L2counter > 10 && L1counter==0 && l2hold==1 && l1hold==0){
+            l2Pressed();
+            L1counter=0;
+            L2counter=0;
+            l1hold=0;
+            l2hold=0;
+          }
+        }
+    }
+    if (ps2x.ButtonPressed(PSB_PAD_UP))  //x refers to the array which will be in terms of 000x 0000 if switch corresponding to up is pressed and up refers to enum
+    { 
+          upPressed();
+          Serial.println('s');
+    }
+    if(ps2x.ButtonReleased(PSB_PAD_UP))
+    {
+       upReleased();
+ //     Serial.println("UR");
+    }
+    
+    if(ps2x.ButtonPressed(PSB_PAD_RIGHT))  //x refers to the array which will be in terms of 000x 0000 if switch corresponding to up is pressed and up refers to enum
+     {
+            rightPressed(); 
+            Serial.println('r');
+     }
+     
+    if(ps2x.ButtonReleased(PSB_PAD_RIGHT))
+     {
+          Serial.println("RR");
+          rightReleased();
+     }
+        
+    if(ps2x.ButtonPressed(PSB_PAD_LEFT))  //x refers to the array which will be in terms of 000x 0000 if switch corresponding to up is pressed and up refers to enum
+     {
+           leftPressed(); 
+           Serial.println('l');
+     }
+    if(ps2x.ButtonReleased(PSB_PAD_LEFT))
+     {
+         Serial.println("LR");         
+         leftReleased();
+     }
+    
+     if(ps2x.ButtonPressed(PSB_PAD_DOWN)) //x refers to the array which will be in terms of 000x 0000 if switch corresponding to up is pressed and up refers to enum
+     {
+          downPressed();
+          Serial.println('D');
+     }
+     
+     if(ps2x.ButtonReleased(PSB_PAD_DOWN)) //x refers to the array which will be in terms of 000x 0000 if switch corresponding to up is pressed and up refers to enum
+     {
+         Serial.println("DR");
+     
+        downReleased();
+     }
+    
+     if(ps2x.ButtonPressed(PSB_SELECT))
+     {  
+          selPressed();
+          Serial.println('E');
+    }
+    if(ps2x.ButtonReleased(PSB_SELECT))
+    {
+          selReleased();
+         Serial.println("ER");     
+    }
+    
+    if(ps2x.ButtonPressed(PSB_START))
+    {  
+            startPressed();
+            Serial.println('S');
+     }
+    
+    if(ps2x.ButtonReleased(PSB_START)) 
+    {
+       Serial.println("SR");    
+        startReleased();
+    }
+    
+    if(ps2x.ButtonPressed(PSB_TRIANGLE))
+    {   
+           trianglePressed();
+            Serial.println('T');     
+    }  
+    
+    if(ps2x.ButtonReleased(PSB_TRIANGLE))
+    {
+       Serial.println("TR");     
+        triangleReleased();
+    }
+    
+    if(ps2x.ButtonPressed(PSB_CIRCLE))
+     {
+            circlePressed();
+            Serial.println('C'); 
+     }
+     
+     if(ps2x.ButtonReleased(PSB_CIRCLE))
+     {
+       Serial.println("CR");
+       circleReleased();
+     }
+   
+     
+     if(ps2x.ButtonReleased(PSB_CROSS))
+     {
+       Serial.println("XR");
+//        flag=0;
+        crossReleased();
+      }
+     
+        
+    
+     if(ps2x.ButtonPressed(PSB_SQUARE))
+     {
+          squarePressed();
+           Serial.println('S');     
+     }
+
+     if(ps2x.ButtonReleased(PSB_SQUARE))
+     {
+       Serial.println("SR");
+     squareReleased();
+     }  
+  
+   
+     
+ 
+   if(ps2x.ButtonPressed(PSB_R1))
+    {
+          r1Pressed();    
+          Serial.println("r1");
+    }
+
+    if(ps2x.ButtonReleased(PSB_R1))
+    {
+         Serial.println("HR");
+         r1Released();
+    }
+    if(ps2x.ButtonPressed(PSB_R2))
+    {
+          r2Pressed();
+          Serial.println("r2");
+    }
+
+    if(ps2x.ButtonReleased(PSB_R2))
+    {
+       Serial.println("IR");
+        r2Released();
+    }
+ delay(wirelessdelay);  
+}
+
+
---
+++ ps2_combination.ino
@@ -0,0 +1,85 @@
+#include <PS2X_lib.h>
+
+#include <SPI.h>
+#define SerialPS2 Serial3
+//#define Newcircuit
+
+
+//-----------------------------------------------DELAYTIME_FOR_PS2------------------------- 
+//#define PS3
+#define basePS2_Y 128.0
+#define wirelessdelay 0
+#ifdef PS3
+#define wirelessdelay 50 
+#define basePS2_Y 127.0
+#endif
+
+
+/* Structs/Variables for driving */
+      
+
+//---------------------------------PINS------------------------------------------------------------
+//------------------------------END-----------------------------------------------------------------------
+
+//----------------------------------------PID-------------------------------------------------------------
+
+//------------------------------PS2---------------------------------------------------------------------
+
+
+
+float distAnalogleft,distAnalogright;
+
+
+#define PS2_DAT        50  //14    
+#define PS2_CMD        51  //15
+#define PS2_SEL        31  //16
+#define PS2_CLK        52  //17
+
+#ifdef Newcircuit
+#define PS2_SEL        6
+#endif
+/******************************************************************
+ * select modes of PS2 controller:
+ *   - pressures = analog reading of push-butttons 
+ *   - rumble    = motor rumbling
+ * uncomment 1 of the lines for each mode selection
+ ******************************************************************/
+//#define pressures   true
+//#define pressures   false
+//#define rumble      true
+//#define rumble      false
+
+PS2X ps2x; // create PS2 Controller Class
+
+double xSquare_leftdistance=0,ySquare_leftdistance=0,xSquare_rightdistance=0,ySquare_rightdistance=0,xCircle_leftdistance=0,yCircle_leftdistance=0,xCircle_rightdistance=0,yCircle_rightdistance=0;
+double LeftAnalogTheta=0,RightAnalogTheta=0;//w1=0,w2=0;
+float LeftAnalogDistance=0,RightAnalogDistance=0;//r1=0,r2=0; //r1-Leftanalogdist w1-Leftanalogtheta
+
+
+int error = 0;
+byte type = 0;
+byte vibrate = 0;
+
+//---------------------------------------------END------------------------------------------------------------------------------------------------
+
+const float rad = 1;
+const float pi = 3.14159;
+
+
+
+
+void setup() {                                                                                           
+  Serial.begin(9600);
+  SerialPS2.begin(9600);
+  initPS2();
+}
+int frontpressed=0,backpressed=0;
+
+void loop() {  
+      getPS2value();    
+      //scalePS2value();
+      PS2executePressed();      
+       
+} 
+
+ 
